19ad152e254a7fb2a93fbdab049bdd5950acb02c
Albert <albert_p@foxmail.com>
2017-12-18 11:08:28 +0800

    Update mysql.js

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: 'rm-wz9v4g5q1q64145i8o.mysql.rds.aliyuncs.com',
-    user: 'albert',
-    password: 'Albert168',
-    database: 'em'
+    host: '',
+    user: '',
+    password: '',
+    database: ''
 }
